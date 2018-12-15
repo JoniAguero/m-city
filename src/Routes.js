f4c8185491d0a6f036985423526632c059d4b9de
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-15 17:03:37 -0300

    Layout

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Layout from './Hoc/Layout';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const Routes = (props) => {
   return(
     <div>
-      hello
+      <Header/>
+      <Layout/>
+      <Footer/>
     </div>
   )
 }
