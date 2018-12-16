8b598be06aad7213ea88154f793cf48616048c52
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-16 13:46:12 -0300

    Before Animation
    Se prepara todo el layout para comenzar a crear la animacion principal

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Layout = (props) => {
   return (
     <div>
-        hello
+        {props.children}
     </div>
   )
 }
--- src/Routes.js
+++ src/Routes.js
@@ -4,15 +4,15 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import { Switch, Route } from 'react-router-dom';
 
+import Home from './Components/Home'
+
 const Routes = (props) => {
   return(
     <div>
       <Header/>
       <Layout>
         <Switch>
-          <Route>
-
-          </Route>
+          <Route exact component={Home} path="/" />
         </Switch>
       </Layout>
       <Footer/>
