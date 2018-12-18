548597345ba56ce27dd86ec8f0f0d4dc7b258003
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-18 11:17:56 -0300

    Edit/Add Matches Form

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Home from './Components/Home'
 import {SignIn} from './Components/Signin';
@@ -10,6 +10,7 @@ import Dashboard from './Components/Admin/Dashboard';
 import PrivateRoute from './Components/AuthRoutes/PrivateRoute';
 import PublicRoute from './Components/AuthRoutes/PublicRoute';
 import AdminMatches from './Components/Admin/Matches';
+import EditMatch from './Components/Admin/Matches/EditMatch';
 
 const Routes = (props) => {
   console.log(props);
@@ -20,6 +21,7 @@ const Routes = (props) => {
         <Switch>
           <PrivateRoute {...props} exact component={Dashboard} path="/dashboard"/>
           <PrivateRoute {...props} exact component={AdminMatches} path="/admin_matches"/>
+          <PrivateRoute {...props} exact component={EditMatch} path="/admin_matches/edit_match/:id"/>
           <PublicRoute {...props} restricted={false} exact component={Home} path="/" />
           <PublicRoute {...props} restricted={true} exact component={SignIn} path="/sign_in" />
         </Switch>
